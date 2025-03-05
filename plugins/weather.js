be5bbaf7db338a81a7c43d11096da7440fd7a088
kyotaka-md45 <botlevanter177@gmail.com>
2025-03-05 22:01:52 +0000

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -1,54 +1,42 @@
-/*
-_  ______   _____ _____ _____ _   _
-| |/ / ___| |_   _| ____/___ | | | |
-| ' / |  _    | | |  _|| |   | |_| |
-| . \ |_| |   | | | |__| |___|  _  |
-|_|\_\____|   |_| |_____\____|_| |_|
-
-ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
-CONTACT ME HERE +237656520674
-YT: KermHackTools
-Github: Kgtech-cmr
-*/
-
 const axios = require('axios');
 const config = require('../config');
 const { cmd, commands } = require('../command');
+
 cmd({
     pattern: "weather",
-    desc: "🌤 Get weather information for a location",
+    desc: "🌤 Obtenez des informations météorologiques pour un emplacement",
     react: "🌤",
     category: "other",
     filename: __filename
 },
 async (conn, mek, m, { from, q, reply }) => {
     try {
-        if (!q) return reply("❗ Please provide a city name. Usage: .weather [city name]");
+        if (!q) return reply("❗ Veuillez fournir un nom de ville. Utilisation : .weather [nom de la ville]");
         const apiKey = '2d61a72574c11c4f36173b627f8cb177'; 
         const city = q;
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         const response = await axios.get(url);
         const data = response.data;
         const weather = `
-🌍 *Weather Information for ${data.name}, ${data.sys.country}* 🌍
-🌡️ *Temperature*: ${data.main.temp}°C
-🌡️ *Feels Like*: ${data.main.feels_like}°C
-🌡️ *Min Temp*: ${data.main.temp_min}°C
-🌡️ *Max Temp*: ${data.main.temp_max}°C
-💧 *Humidity*: ${data.main.humidity}%
-☁️ *Weather*: ${data.weather[0].main}
-🌫️ *Description*: ${data.weather[0].description}
-💨 *Wind Speed*: ${data.wind.speed} m/s
-🔽 *Pressure*: ${data.main.pressure} hPa
+🌍 *Informations météo pour ${data.name}, ${data.sys.country}* 🌍
+🌡️ *Température* : ${data.main.temp}°C
+🌡️ *Ressenti* : ${data.main.feels_like}°C
+🌡️ *Température min* : ${data.main.temp_min}°C
+🌡️ *Température max* : ${data.main.temp_max}°C
+💧 *Humidité* : ${data.main.humidity}%
+☁️ *Météo* : ${data.weather[0].main}
+🌫️ *Description* : ${data.weather[0].description}
+💨 *Vitesse du vent* : ${data.wind.speed} m/s
+🔽 *Pression* : ${data.main.pressure} hPa
 
-*POWERED BY KG TECH*
+*MERCI D'AVOIR UTILISÉ DRACULA*
 `;
         return reply(weather);
     } catch (e) {
         console.log(e);
         if (e.response && e.response.status === 404) {
-            return reply("🚫 City not found. Please check the spelling and try again.");
+            return reply("🚫 Ville non trouvée. Veuillez vérifier l'orthographe et réessayer.");
         }
-        return reply("⚠️ An error occurred while fetching the weather information. Please try again later.");
+        return reply("⚠️ Une erreur s'est produite lors de la récupération des informations météo. Veuillez réessayer plus tard.");
     }
 });
